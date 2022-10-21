3ab16a6291a6a6e9274d778b33fd48cc83582c8c
franconoronha <franconoronha11@gmail.com>
2022-10-20 22:30:50 -0300

    versao final

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QImage, QPixmap, qRgb, QIcon
+from PyQt5.QtGui import QImage, QPixmap, qRgb, QIcon, QFont
 from PyQt5.QtCore import Qt
 from ctypes import *
 import sys
 import os
-import time
 
+# Carrega a biblioteca em C++ levando em conta o SO
 if sys.platform == "win32":
     lib_file = "\\shared.dll"
 else:
@@ -13,7 +13,9 @@ else:
 
 path = os.getcwd() + lib_file
 shared = CDLL(path)
+#
 
+# Define os tipos dos argumentos e retornos das funções da biblioteca, para conversão automática
 shared.createArray.argtypes = [POINTER((POINTER(c_int))), c_int]
 shared.createArray.restype = None
 
@@ -22,111 +24,125 @@ shared.calcArrayMandelbrot.restype = None
 
 shared.release.argtypes = [POINTER(c_int)]
 shared.release.restype = None
+#
 
-
+# Definindo uma tela principal extendendo a classe padrão
 class TelaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.modo = True
-
+        
+        # Definição dos elementos básicos da tela
         x_pos, y_pos, width, height = 300, 300, 600, 600 # manter width = height
-
-        self.setGeometry(x_pos, y_pos, width, height)
+        self.setGeometry(x_pos, y_pos, width, height + 100)
         self.setWindowTitle("Fractal de Mandelbrot")
         self.setWindowIcon(QIcon("icon.png"))
+        #
 
+        # Condição inicial do fractal
         self.limite_superior, self.limite_inferior = 2, -2
         self.offset_x, self.offset_y = 0, 0
+        #
 
+        # Criação do elemento de imagem(vazio)
         self.imageDisplay = QLabel(self)
         self.imageDisplay.setGeometry(0, 0, width, height)
-
         self.img = QImage(width, height, QImage.Format_RGB32)
-    
+        #
+
+        # Criação e definição das labels da legenda 
+        self.label_1 = QLabel("W: mover para cima | A: mover para esquerda | D: mover para direta | S: mover para baixo", self)
+        self.label_2 = QLabel("Z: zoom-in | X: zoom-out | C: zoom-in grande | V: zoom-out grande | R: reset",self)
+        self.label_1.setFont(QFont('Arial', 10))
+        self.label_2.setFont(QFont('Arial', 10))
+        self.label_1.setGeometry(0, height + 10, width, 40)
+        self.label_2.setGeometry(0, height + 50, width, 40)
+        self.label_1.setAlignment(Qt.AlignCenter)
+        self.label_2.setAlignment(Qt.AlignCenter)
+        #
+
+        # Criação e alocação do array que é compartilhado entre C++ e Python
         self.arrayMandelbrot = POINTER(c_int)()
         shared.createArray(self.arrayMandelbrot, width * height)
+        #
 
+    # Função que coloca pixel por pixel da imagem
     def draw(self):
         largura_total = self.limite_superior - self.limite_inferior
         base_x = self.limite_inferior + self.offset_x
         base_y = self.limite_superior + self.offset_y
         size = self.img.height() # largura === altura
 
+        # Atualiza o array compartilhado com os parâmetros atuais
         shared.calcArrayMandelbrot(self.arrayMandelbrot, base_x, base_y, size, largura_total)
 
+        # Percore o array e coloca o valor em RGB na matriz de pixeis
         for x in range(size):
             for y in range(size):
                 val = self.arrayMandelbrot[x * size + y]
                 self.img.setPixel(x, y, qRgb(val, val, val))
 
+        # Passa essa matriz para o elemento de display de imagem
         pixmap = QPixmap.fromImage(self.img)
         self.imageDisplay.setPixmap(pixmap)
         
+    # Trata os comandos do teclado (calcula novo parâmetro e redesenha)
     def keyPressEvent(self, e):
+        # Mover para cima
         if e.key() == Qt.Key_W:
-            start = time.time()
             self.offset_y += self.limite_superior * 0.1
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Mover para esquerda
         elif e.key() == Qt.Key_A:
-            start = time.time()
             self.offset_x -= self.limite_superior * 0.1
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Mover para baixo
         elif e.key() == Qt.Key_S:
-            start = time.time()
             self.offset_y -= self.limite_superior * 0.1
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Mover para direita
         elif e.key() == Qt.Key_D:
-            start = time.time()
             self.offset_x += self.limite_superior * 0.1
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Zoom-in
         elif e.key() == Qt.Key_Z:
-            start = time.time()
             self.limite_inferior -= self.limite_inferior * 0.05
             self.limite_superior -= self.limite_superior * 0.05
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Zoom-out
         elif e.key() == Qt.Key_X:
-            start = time.time()
             self.limite_inferior += self.limite_inferior * 0.05
             self.limite_superior += self.limite_superior * 0.05
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Zoom-in grande
         elif e.key() == Qt.Key_C:
-            start = time.time()
             self.limite_inferior -= self.limite_inferior * 0.8
             self.limite_superior -= self.limite_superior * 0.8
             self.draw()
-            end = time.time()
-            print(end - start)
+
+        # Zoom-out grande
         elif e.key() == Qt.Key_V:
-            start = time.time()
             self.limite_inferior += self.limite_inferior * 0.8
             self.limite_superior += self.limite_superior * 0.8
             self.draw()
-            end = time.time()
-            print(end - start)
-        elif e.key() == Qt.Key_T:
-            self.modo = not self.modo
-            self.draw()
+
+        # Reset
         elif e.key() == Qt.Key_R:
             self.offset_x, self.offset_y = 0, 0
             self.limite_superior, self.limite_inferior = 2, -2
             self.draw()
 
+    # Ao fechar a janela, desalocar array compartilhado
     def closeEvent(self, event):
         shared.release(self.arrayMandelbrot)
         event.accept()
 
+# Criação da tela
 def main():
     app = QApplication(sys.argv)
     win = TelaPrincipal()
--- shared.cpp
+++ shared.cpp
@@ -1,3 +1,4 @@
+// adiciona a diretiva de exportação ao arquivo objeto no windows
 #if defined(_WIN32)
 #  define DLL_EXPORT __declspec(dllexport)
 #else
@@ -7,30 +8,36 @@
 #include "shared.hpp"
 #include <stdlib.h>
 #include <cmath>
-#include <iostream>
 
+// desabilita o name mangling do C++ dentro do bloco
 extern "C" {
+  // Função para alocar um array compartilhado em memória
   DLL_EXPORT void createArray(int** input, int size) {
     int* p = (int*) malloc (size * sizeof(int));
    *input = p;
   }
 
+  // Função para desalocar um array compartilhado em memória
   DLL_EXPORT void release(int* input) {
     free(input);
   }
 
+  // Calcula o valor de cada pixel do array com base no tamanho do plano cartesiano dos números complexos
   DLL_EXPORT void calcArrayMandelbrot(int* input, double base_x, double base_y, int size, double largura_total) {
-    double step_size = largura_total / size;
+    double step_size = largura_total / size; // Tamanho do intervalo que o pixel representa
     double ponto_x, ponto_y, zr, zi, temp;
 
-    int index, count, result;
+    int index, count;
 
     for(int i = 0; i < size; ++i) {
       for(int j = 0; j < size; ++j) {
         index = i * size + j;
+        // Definição do ponto no plano cartesiano
         ponto_x = base_x + i * step_size;
         ponto_y = base_y - j * step_size;
 
+        // Verificar em quantas iterações tende ao inifito
+        // Max iterações = 100
         count = 0;
         zr = 0.0, zi = 0.0;
 
@@ -41,8 +48,7 @@ extern "C" {
             ++count;
         } while (count < 100 && zr * zr + zi * zi < 4.0);
 
-        result = floor((count / 100.0) * 255);
-        input[index] = result;
+        input[index] = floor((count / 100.0) * 255); // transformado para faixa de 0 a 255
       }
     }
    }
